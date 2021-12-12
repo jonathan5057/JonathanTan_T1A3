357382c7c3c3ac4fc9806a1114f74b7c5662a741
jonathan5057 <GCAB022114@coderacademy.edu.au>
2021-12-12 23:03:40 +1000

    create array list for the pizza menu, create print_selection method for the main menu

--- src/main.rb
+++ src/main.rb
@@ -1,16 +1,24 @@
 require "tty-prompt"
+require "tty-table"
+
 
 # Global Variable assigned for tty-prompt
 $prompt = TTY::Prompt.new 
 
 # App Launch with welcoming message
-puts "Welcome to Mr.Pizza!!!"
+puts "Welcome to Pizza Boss!!!"
 
-# def initialise()
-#     @Category = {}
-#     @food = {}
-#     @prie = {}
-# end
+
+# Creat the food menu for pizza  
+FOOD_LIST = {
+    "Hwaiian" => 4.0,
+    "BBQ Cheeseburger" => 3.0,
+    "Peri Peri Chicken" => 4.5,
+    "Peperoni" => 3.0,
+    "Chicken Tikamasala" => 3.0,
+    "Double Bacon" => 7.0,
+    "Beef slide" => 8.0
+}
 
 def clear()
     # This method clear the terminal.
@@ -44,24 +52,63 @@ end
 
 def print_selection()
     # This method prints the main menu of the system. 
-    
-    selection = $prompt.select("Please select option!", ["ADD ORDER", "REMOVE ORDER", "SHOW DETAILS", "EXIT"])
+    confirmed_orders = []
+        
+    while true
+        selection = $prompt.select("Please select option!", ["ADD ORDER", "REMOVE ORDER", "SHOW DETAILS", "FINALIZED ORDER", "EXIT"])
+        
+        case selection
+        when 'ADD ORDER'
+            clear
+            confirmed_orders << add_order()
+        when 'SHOW DETAILS'
+            clear
+            show_order(confirmed_orders)
+        when 'REMOVE ORDER'
+            clear
+            remove_order(confirmed_orders).each do |order|
+                puts order
+                confirmed_orders.delete_at(order)
+            end
+        when "FINALIZED ORDER"
+            clear
+            puts "Here is your total bill: "
+            puts confirmed_orders.map{|order| order[1]}.sum
+        else
+            exit
+        end
+    end
     return selection
 end
 
+
+# def get_total()
+#     # This mehod gets the total value of the order.
+#     order.each do |result, order| 
+# end
+
 def add_order()
     # This method print the food menu and gets items from user.
-
+    selected_food = $prompt.select("Please select option!", FOOD_LIST.keys)
+    [selected_food, FOOD_LIST[selected_food]]
 end
 
-def remove_order()
+def remove_order(orders)
     # This method delete item from the menu list
+    count = 0
+    choices = orders.reduce({}) do |result, order|
+        result["#{order}-#{count}"] = count
+        count +=1
 
+        result
+    end
+    $prompt.multi_select("Select order to remove?", choices)
 end
 
-def show_detail()
+def show_order(orders)
     # This method returns the full ordering list 
-    
+    table = TTY::Table.new(["food","price"], orders)
+    puts table.render(:ascii)
 end
 
 option = ""
@@ -83,9 +130,8 @@ while option != "Exit"
             puts "Welcome to your local Calamvale Pizza Store!"
             print_selection
         else
-            puts "See you next time.. #{name}!!"
-            break
-            
+            puts "See you next time.. !!"
+            break       
     end
     clear      
 end

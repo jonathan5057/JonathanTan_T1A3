06d3e7704d40e8e955e0f27e209dc5cd341336e5
jonathan5057 <GCAB022114@coderacademy.edu.au>
2021-12-18 20:14:40 +1000

    fix the user_name.rb file

--- src/user_name.rb
+++ src/user_name.rb
@@ -1,15 +1,5 @@
-# def gets_time_after(seconds: 900)
-#     # Prints actual time for the next 15 minutes
-
-#     Time.now + seconds
-#     # set time to next 15 minutes (900 seconds)
-# end
-
-# def format_time(time, format: '%I:%M%p')
-#     time.strftime(format)
-# end     
-
-# Separted this function from my main.rb for the purpose of testing
+ # Separted this method from my main.rb for the purpose of testing
+ # To test the user input name
 
 def invalid_name(name)
     name.length > 10 || name.length == 0 || name.match(/[^A-Za-z]/)

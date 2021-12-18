6bb9a0bcfa215cc565334122a9753e3f70454e4e
jonathan5057 <GCAB022114@coderacademy.edu.au>
2021-12-18 11:35:55 +1000

    refactoring - moved hashes of pizza list to a new ruby file menu.rb

--- src/main.rb
+++ src/main.rb
@@ -2,22 +2,14 @@ require "tty-prompt"
 require "tty-table"
 # require "colorize"
 
+
+require_relative "./menu.rb"
+
 # Global Variable assigned for tty-prompt
 $prompt = TTY::Prompt.new 
 
 # App Launch with welcoming message
 puts "Welcome to Pizza Boss!!!"
-
-# Create hashes of food menu for pizza  
-FOOD_LIST = {
-    "Hwaiian" => 4.0,
-    "BBQ Cheeseburger" => 3.0,
-    "Peri Peri Chicken" => 4.5,
-    "Peperoni" => 3.0,
-    "Chicken Tikamasala" => 3.0,
-    "Double Bacon" => 7.0,
-    "Beef slide" => 8.0
-}
 
 def clear()
     # This method clear the terminal.
---
+++ src/menu.rb
@@ -0,0 +1,10 @@
+# Create hashes of food menu for pizza  
+FOOD_LIST = {
+    "Hwaiian" => 4.0,
+    "BBQ Cheeseburger" => 3.0,
+    "Peri Peri Chicken" => 4.5,
+    "Peperoni" => 3.0,
+    "Chicken Tikamasala" => 3.0,
+    "Double Bacon" => 7.0,
+    "Beef slide" => 8.0
+}
